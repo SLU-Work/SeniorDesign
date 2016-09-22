6b7f101da6cb801c33feb7476e14fd01a80acd29
Maggie Schroeder <maggieschroeder@apples-MacBook-Pro-2.local>
2016-09-22 16:38:47 -0500

    Test[RS]

--- readimages.m
+++ readimages.m
@@ -9,10 +9,10 @@
 
 % A = imread('Iphone5 - pole - 1.jpg');
 % A1 = rgb2gray(A);
-
-A = imrotate(imread('Iphone5 - stairs - 3.jpg'),-90);
+tic
+A = imread('S5-bench-3.jpg');
 A1 = rgb2gray(A);
-A1 = wiener2(A1,[10 10]);
+A1 = wiener2(A1,[20 20]);
 %A1 = imcomplement(A1);
 %A1 = A1/max(max(A1));
 % BW1 = edge(A1,'sobel');
@@ -23,15 +23,17 @@ A1 = wiener2(A1,[10 10]);
 % [Gmag, Gdir] = imgradient(A1,'prewitt');
 
 BW2 = edge(A1,'canny');
-% R = 10;
-% N = 4;
-% SE = strel('disk',R,N);
-% SE2 = strel('rectangle',[4 2]);
+%R = 4;
+%N = 4;
+%SE = strel('disk',R,N);
+SE2 = strel('rectangle',[4 2]);
 % SE3 = strel('diamond',R);
 % Edgelinked1 = imdilate(BW2,SE);
 % Edgelinked3 = imdilate(BW2,SE3);
 
-SE2 = strel('rectangle',[4 2]);
+% SE2 = strel('rectangle',[4 2]);
 Edgelinked2 = imdilate(BW2,SE2);
-% CC = bwconncomp(Edgelinked1);
-% L = bwlabel(Edgelinked1,4);
+CC = bwconncomp(Edgelinked2);
+stats = regionprops(CC,'FilledImage');
+L = bwlabel(Edgelinked2,4);
+toc
